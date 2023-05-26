9a3645c793c5e94e81be5d245b52625e4d124dc4
Shreyash Halge <shreyash.shalge@gmail.com>
2023-05-27 03:44:50 +0530

    Update a5.py

--- lp2/a5.py
+++ lp2/a5.py
@@ -1,121 +1,75 @@
-class Node:
-    def __init__(self,data,level,fval):
-        """ Initialize the node with the data, level of the node and the calculated fvalue """
-        self.data = data
-        self.level = level
-        self.fval = fval
+import re
+import random
 
-    def generate_child(self):
-        """ Generate child nodes from the given node by moving the blank space
-            either in the four directions {up,down,left,right} """
-        x,y = self.find(self.data,'_')
-        """ val_list contains position values for moving the blank space in either of
-            the 4 directions [up,down,left,right] respectively. """
-        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
-        children = []
-        for i in val_list:
-            child = self.shuffle(self.data,x,y,i[0],i[1])
-            if child is not None:
-                child_node = Node(child,self.level+1,0)
-                children.append(child_node)
-        return children
-        
-    def shuffle(self,puz,x1,y1,x2,y2):
-        """ Move the blank space in the given direction and if the position value are out
-            of limits the return None """
-        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
-            temp_puz = []
-            temp_puz = self.copy(puz)
-            temp = temp_puz[x2][y2]
-            temp_puz[x2][y2] = temp_puz[x1][y1]
-            temp_puz[x1][y1] = temp
-            return temp_puz
-        else:
-            return None
-            
+rules = {
+    'greeting': {
+        'patterns': [r'hello', r'hi', r'hey'],
+        'responses': ['Hello! Welcome to our food ordering service.', 'Hi there! How can I assist you with your order?']
+    },
+    'menu': {
+        'patterns': [r'menu', r'options'],
+        'responses': ['Sure! Here is our menu: ...\n 1.Pav Bhaji \n 2.Vada Pav \n 3.Maggi \n 4.Chai'],
+    },
+    'food items' : {
+        'patterns' : [r'Vada Pav',r'Pav Bhaji',r'Chai',r'Maggi' ],
+        'responses' : ['Do you want to confirm your order??'],
+    },
+    'order': {
+        'patterns': [r'order', r'I want to order'],
+        'responses': ['Great! Please let me know what items you would like to order.'],
+    },
+    'customization': {
+        'patterns': [r'customize', r'special request'],
+        'responses': ['Certainly! Let me know your specific requirements or any dietary restrictions.'],
+    },
+    'confirm_order': {
+        'patterns': [r'confirm', r'place order'],
+        'responses': ['Perfect! Your order has been placed. The estimated delivery time is approximately 30 minutes.'],
+    },
+    'gratitude' : {
+        'patterns' : [r'thank you'],
+        'responses' : ['My pleasure']
+    },
+    'cancel_order': {
+        'patterns': [r'cancel', r'change', r'update'],
+        'responses': ['I apologize for the inconvenience. Please contact our customer support for order modifications.'],
+    },
+    'goodbye': {
+        'patterns': [r'bye', r'goodbye', r'see you'],
+        'responses': ['Thank you for choosing our food ordering service. Ab nikal BC yaha se', 'Goodbye!'],
+    },
+    'default': {
+        'responses': ['Im sorry, I didnt understand that. Can you please rephrase?']
+    }
+}
 
-    def copy(self,root):
-        """ Copy function to create a similar matrix of the given node"""
-        temp = []
-        for i in root:
-            t = []
-            for j in i:
-                t.append(j)
-            temp.append(t)
-        return temp    
-            
-    def find(self,puz,x):
-        """ Specifically used to find the position of the blank space """
-        for i in range(0,len(self.data)):
-            for j in range(0,len(self.data)):
-                if puz[i][j] == x:
-                    return i,j
-
-
-class Puzzle:
-    def __init__(self,size):
-        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
-        self.n = size
-        self.open = []
-        self.closed = []
-
-    def accept(self):
-        """ Accepts the puzzle from the user """
-        puz = []
-        for i in range(0,self.n):
-            temp = input().split(" ")
-            puz.append(temp)
-        return puz
-
-    def f(self,start,goal):
-        """ Heuristic Function to calculate hueristic value f(x) = h(x) + g(x) """
-        return self.h(start.data,goal)+start.level
-
-    def h(self,start,goal):
-        """ Calculates the different between the given puzzles """
-        temp = 0
-        for i in range(0,self.n):
-            for j in range(0,self.n):
-                if start[i][j] != goal[i][j] and start[i][j] != '_':
-                    temp += 1
-        return temp
-        
-
-    def process(self):
-        """ Accept Start and Goal Puzzle state"""
-        print("Enter the start state matrix \n")
-        start = self.accept()
-        print("Enter the goal state matrix \n")        
-        # goal = self.accept()
-        goal = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '_']]
-
-        start = Node(start,0,0)
-        start.fval = self.f(start,goal)
-        """ Put the start node in the open list"""
-        self.open.append(start)
-        print("\n\n")
-        while True:
-            cur = self.open[0]
-            print("")
-            print("  | ")
-            print("  | ")
-            print(" \\\'/ \n")
-            for i in cur.data:
-                for j in i:
-                    print(j,end=" ")
-                print("")
-            """ If the difference between current and goal node is 0 we have reached the goal node"""
-            if(self.h(cur.data,goal) == 0):
-                break
-            for i in cur.generate_child():
-                i.fval = self.f(i,goal)
-                self.open.append(i)
-            self.closed.append(cur)
-            del self.open[0]
-
-            """ sort the opne list based on f value """
-            self.open.sort(key = lambda x:x.fval,reverse=False)
+def match_patterns(user_input, patterns):
+    for pattern in patterns:
+        match = re.search(pattern, user_input, re.IGNORECASE)
+        if match:
+            return True
+    return False
 
 
-puz = Puzzle(3)
-puz.process()
+def get_response(user_input):
+    for intent, data in rules.items():
+        patterns = data.get('patterns')
+        if patterns and match_patterns(user_input, patterns):
+            responses = data.get('responses')
+            return random.choice(responses)
+    return random.choice(rules['default']['responses'])
+
+
+
+def chat():
+    print("Chatbot: Welcome to our food ordering service!")
+    while True:
+        user_input = input("User: ")
+        response = get_response(user_input)
+        print("Chatbot: ", response)
+
+        if any(re.search(pattern, user_input) for pattern in rules['goodbye']['patterns']):
+            break
+
+
+chat()
